1f9cda862e956f055862488271304148815c5577
David Hoppenbrouwers <onder@demindiro.com>
2025-12-11 23:01:43 +0100

    onder::net::Ip4: add constructor for 4 byte parts

--- include/onder/net.hpp
+++ include/onder/net.hpp
@@ -11,6 +11,9 @@ struct Ip4 {
 	uint32_t addr;
 
 	Ip4() : addr(0) {}
+	Ip4(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+		: addr(((uint32_t)a << 24) | ((uint32_t)b << 16) | ((uint32_t)c << 8) | (uint32_t)d)
+	{}
 };
 
 struct Ip6 {
